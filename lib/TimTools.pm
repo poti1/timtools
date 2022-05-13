86554e6fdfecfd31aa6922367c4d78b6fd4e37eb
Timofey Potapov <timofey.potapov@otrs.com>
2022-05-13 15:05:38 +0200

    Init

---
+++ lib/TimTools.pm
@@ -0,0 +1,91 @@
+package TimTools;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+TimTools - Some common functions.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use TimTools;
+
+    my $foo = TimTools->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head1 AUTHOR
+
+Tim Potapov, C<< <tim.potapov[AT]gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-timtools at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=TimTools>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc TimTools
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=TimTools>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/TimTools>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/TimTools>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Tim Potapov.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of TimTools
